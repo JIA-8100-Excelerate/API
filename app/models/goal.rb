d45e1adc545105dcf048572e26d9c031b65836c9
Zack Mudge <zmudge@gatech.edu>
2018-10-31 17:28:29 -0400

    Limit scope for Goal category to student

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates_presence_of :category, :student
-  validates_uniqueness_of :category, :case_sensitive => false
+  validates_uniqueness_of :category, :case_sensitive => false, :scope => :student
 end
