af78ec88ed3c2289e4ce039a3cf6d23734c5adf4
Zack Mudge <zmudge@gatech.edu>
2018-10-31 14:13:30 -0400

    Validates unique Goal category, Task name

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates_presence_of :category, :student
+  validates_uniqueness_of :category, :case_sensitive => false
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :goal
   validates_presence_of :name
+  validates_uniqueness_of :name, :case_sensitive => false
 end
