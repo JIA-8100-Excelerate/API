26e193dc72b24e33405c077cc89d39019820a5ad
Zack Mudge <zmudge@gatech.edu>
2018-10-31 16:17:49 -0400

    Limit uniqueness validation scope for task name to goal_id

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :goal
   validates_presence_of :name
-  validates_uniqueness_of :name, :case_sensitive => false
+  validates_uniqueness_of :name, :case_sensitive => false, :scope => :goal_id
 end
