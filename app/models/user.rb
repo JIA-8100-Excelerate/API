c409b7aed4099de59ffbc8e3d6c387372af2e597
Zack Mudge <zmudge@gatech.edu>
2018-11-12 13:55:42 -0500

    Remove presence validation for mentor field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :goals, foreign_key: :student
-  validates_presence_of :firstname, :lastname, :email, :password_digest, :mentor
+  validates_presence_of :firstname, :lastname, :email, :password_digest
   validates_uniqueness_of :email
 end
